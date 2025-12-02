9ccf8440a4076b7571c262ccceaaf1c751b12e69
alessandro-giuzio <giuzio@icloud.com>
2025-12-02 15:44:45 +0100

    Add notes functionality with dedicated routes and views for displaying notes and individual note details

---
+++ php/controllers/notes.php
@@ -0,0 +1,14 @@
+<?php
+$config = require('config.php');
+$db = new Database($config['database']);
+
+
+$heading = 'My Notes Page';
+
+
+$notes = $db->query('select * from notes where user_id = 1')->fetchAll();
+
+
+
+
+require "views/notes.view.php";
---
+++ php/views/note.view.php
@@ -0,0 +1,15 @@
+<?php require('partials/head.php') ?>
+<?php require('partials/nav.php') ?>
+<?php require('partials/banner.php') ?>
+
+<main>
+  <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+
+    <p><?= $note['body'] ?></p>
+    <p class="mb-6">
+      <a href="/notes" class="text-red-800 underline">go back...</a>
+    </p>
+  </div>
+</main>
+
+<?php require('partials/footer.php') ?>
