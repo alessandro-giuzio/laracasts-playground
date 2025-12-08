00d53b3c24ccebe6cbea31c87eb45c2bbd0a4fd1
Alessandro Giuzio <38163055+alessandro-giuzio@users.noreply.github.com>
2025-12-08 16:00:06 +0100

    Implement response handling and authorization checks for notes access

--- php/Database.php
+++ php/Database.php
@@ -4,7 +4,7 @@ class Database
 {
   public $connection;
 
-  public function __construct($config, $username = 'root', $password = 'evitaerc')
+  public function __construct($config, $username = 'root', $password = '')
   {
     $dsn = 'mysql:' . http_build_query($config, '', ';');
 
--- php/controllers/note.php
+++ php/controllers/note.php
@@ -5,7 +5,19 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 $heading = 'Note';
+$currentUserId = 1; // Simulating a logged-in user with ID 1
 
-$note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->fetch();
+$note = $db->query('select * from notes where id = :id', [
+  'id' => $_GET['id']
+])->fetch();
+
+if (! $note) {
+  abort();
+}
+
+
+if ($note['user_id'] !== $currentUserId) {
+  abort(Response::FORBIDDEN);
+}
 
 require "views/note.view.php";
