e994ffe8933608dc6cbe5aea3e9ccbefb00912da
Alessandro Giuzio <38163055+alessandro-giuzio@users.noreply.github.com>
2025-12-08 16:24:07 +0100

    Refactor Database class to store prepared statements and enhance query methods; update note controllers for improved error handling and authorization checks.

--- php/controllers/note.php
+++ php/controllers/note.php
@@ -1,23 +1,15 @@
-
 <?php
-$config = require('config.php');
 
+$config = require('config.php');
 $db = new Database($config['database']);
 
 $heading = 'Note';
-$currentUserId = 1; // Simulating a logged-in user with ID 1
+$currentUserId = 1;
 
 $note = $db->query('select * from notes where id = :id', [
   'id' => $_GET['id']
-])->fetch();
+])->findOrFail();
 
-if (! $note) {
-  abort();
-}
-
-
-if ($note['user_id'] !== $currentUserId) {
-  abort(Response::FORBIDDEN);
-}
+authorize($note['user_id'] === $currentUserId);
 
 require "views/note.view.php";
