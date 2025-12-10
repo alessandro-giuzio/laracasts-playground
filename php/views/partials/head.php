5638bb6ed16a6cbaec44b4a46265b69b792dac43
alessandro-giuzio <giuzio@icloud.com>
2025-12-10 11:37:33 +0100

    Refactor Database constructor to set default password; update routing to include note creation and improve 403 view styling

--- php/views/partials/head.php
+++ php/views/partials/head.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../../styles.css">
-  <script src="https://cdn.tailwindcss.com"></script>
+  <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
   <!--   <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> -->
 
   <title>Demo</title>
