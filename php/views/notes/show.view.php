fbc0dffe611e8fdee7abb56bc796cc36da83e755
alessandro-giuzio <giuzio@icloud.com>
2025-12-29 18:59:10 +0100

    feat: add note editing functionality with update and delete options

--- php/views/notes/show.view.php
+++ php/views/notes/show.view.php
@@ -4,16 +4,16 @@
 
 <main>
   <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+    <p class="mb-6">
+      <a href="/notes" class="text-blue-500 underline">go back...</a>
+    </p>
 
     <p><?= htmlspecialchars($note['body']) ?></p>
-    <p class="mt-6">
-      <a href="/notes" class="text-red-800 underline">go back...</a>
-    </p>
-  <form class="mt-6" method="POST">
-            <input type="hidden" name="_method" value="DELETE">
-            <input type="hidden" name="id" value="<?= $note['id'] ?>">
-    <button class="text-sm text-red-500 rounded-lg border border-red-500 px-3 py-1 bg-red-100">Delete</button>
-  </form>
+
+    <footer class="mt-6">
+      <a href="/note/edit?id=<?= $note['id'] ?>"
+        class="inline-flex justify-center rounded-md border border-transparent bg-gray-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Edit</a>
+    </footer>
   </div>
 </main>
 
