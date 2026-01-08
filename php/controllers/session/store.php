3d45f5c39e778f7bffe290b3243e14b03f66b9b0
alessandro-giuzio <giuzio@icloud.com>
2026-01-08 15:52:05 +0100

    feat: implement user authentication with login and logout functionality, add middleware for guest and authenticated routes

--- php/controllers/session/store.php
+++ php/controllers/session/store.php
@@ -14,12 +14,12 @@ if (!Validator::email($email)) {
   $errors['email'] = 'Please provide a valid email address.';
 }
 
-if (!Validator::string($password, 7, 255)) {
-  $errors['password'] = 'Please provide a password of at least seven characters.';
+if (!Validator::string($password)) {
+  $errors['password'] = 'Please provide a valid password.';
 }
 
 if (!empty($errors)) {
-  return view('registration/create.view.php', [
+  return view('session/create.view.php', [
     'errors' => $errors
   ]);
 }
@@ -29,16 +29,18 @@ $user = $db->query('select * from users where email = :email', [
 ])->find();
 
 if ($user) {
-  header('location: /');
-  exit();
-} else {
-  $db->query('INSERT INTO users(email, password) VALUES(:email, :password)', [
-    'email' => $email,
-    'password' => password_hash($password, PASSWORD_BCRYPT)
-  ]);
+  if (password_verify($password, $user['password'])) {
+    login([
+      'email' => $email
+    ]);
 
-  login($user);
-
-  header('location: /');
-  exit();
+    header('location: /');
+    exit();
+  }
 }
+
+return view('session/create.view.php', [
+  'errors' => [
+    'email' => 'No matching account found for that email address and password.'
+  ]
+]);
